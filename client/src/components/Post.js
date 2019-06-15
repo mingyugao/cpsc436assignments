68a7d4a33c03f8b71bd6d4da2fd4339bb865e5d0
Mingyu Gao <mingyu.gao96@gmail.com>
2019-06-15 03:55:30 -0700

    refactor post and add post styling

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { mapStateToPropsPost, mapDispatchToPropsPost } from '../reduxMaps';
 
-const Post = ({
-  id,
-  title,
-  content,
-  edited,
-  upvotes,
-  openEditPostModal,
-  deletePostRequest
-}) => (
-  <div className="post">
-    <div>{title}</div>
-    <div>{content}</div>
-    {edited ? <div>edited</div> : ''}
-    <div>{upvotes}</div>
-    <button onClick={() => openEditPostModal(id)}>Edit</button>
-    <button onClick={() => deletePostRequest(id)}>Delete</button>
-  </div>
-);
+class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isExpanded: true };
+    this.toggleExpanded = this.toggleExpanded.bind(this);
+  }
+
+  toggleExpanded() {
+    this.setState({ isExpanded: !this.state.isExpanded });
+  }
+
+  render() {
+    const {
+      id,
+      title,
+      content,
+      edited,
+      upvotes,
+      openEditPostModal,
+      deletePostRequest
+    } = this.props;
+
+    return this.state.isExpanded
+      ? (
+        <div className="post">
+          <h3 onClick={() => this.toggleExpanded()}>{title}</h3>
+          <div>{content}</div>
+          {edited ? <div>(Edited)</div> : <div></div>}
+          <div>{upvotes} upvotes</div>
+          <span>
+            [<button onClick={() => openEditPostModal(id)}>Edit</button>]
+          </span>
+          <span>
+            [<button onClick={() => deletePostRequest(id)}>Delete</button>]
+          </span>
+        </div>
+      )
+      : (
+        <div className="post">
+          <h3 onClick={() => this.toggleExpanded()}>{title}</h3>
+        </div>
+      );
+  }
+}
 
 export default connect(mapStateToPropsPost, mapDispatchToPropsPost)(Post);
