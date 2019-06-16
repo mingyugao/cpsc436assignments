83430d2cd8aeac80f705dee7222326fd3186ef2f
Mingyu Gao <mingyu.gao96@gmail.com>
2019-06-16 04:51:12 -0700

    adjust styling, add styling for modal

--- client/src/components/EditPostModal.js
+++ client/src/components/EditPostModal.js
@@ -16,28 +16,34 @@ const EditPostModal = ({
   closeEditPostModal,
   editPostRequest
 }) => {
+  document.body.style.overflow = isOpen ? 'hidden' : 'unset';
+
   return isOpen
     ? (
       <div className="edit-post-modal">
-        <input
-          disabled={!isEditable}
-          type="text"
-          value={title}
-          onChange={e => onChangeTitle(e.target.value)}
-        />
-        <input
-          disabled={!isEditable}
-          type="text"
-          value={content}
-          onChange={e => onChangeContent(e.target.value)}
-        />
-        <button onClick={() => closeEditPostModal()}>Cancel</button>
-        <input
-          disabled={!isEditable}
-          type="button"
-          value="Submit"
-          onClick={() => editPostRequest({ id, title, content })}
-        />
+        <div>
+          <div>Edit post</div>
+          <input
+            disabled={!isEditable}
+            type="text"
+            value={title}
+            onChange={e => onChangeTitle(e.target.value)}
+          />
+          <textarea
+            disabled={!isEditable}
+            cols="40"
+            rows="10"
+            value={content}
+            onChange={e => onChangeContent(e.target.value)}
+          ></textarea>
+          <div>
+            <button onClick={() => closeEditPostModal()}>CANCEL</button>
+            <button
+              disabled={!isEditable || !title || !content}
+              onClick={() => editPostRequest({ id, title, content })}
+            >SUBMIT</button>
+          </div>
+        </div>
       </div>
     )
     : '';
